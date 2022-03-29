8e0421be8dd51afc1c4e4e7f38fee50113c1dc8c
Nick <nickdloucks@outlook.com>
2022-03-28 20:53:08 -0700

    middleware & fsPromises: practicing syntax

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
-import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import express  from 'express';
+import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import e from 'express';
+import express from 'express';
+import { promises as fsPromises } from fs;
 
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
 });
+
+const resize = function(req: express.Request, res: express.Response, next: Function): void {
+    writePic();
+    next();
+}
+const writePic = async ()=>{
+    try{
+        let smallerPic = await fsPromises.writeFile('./images/fjord.jpg?width=200&height=200')
+    }catch(err){
+        console.log(err);
+    }
+}
+
+app.get('/images/fjord.jpg', resize, (req, res)=> {
+    resize(req, res, res.send)
+})
