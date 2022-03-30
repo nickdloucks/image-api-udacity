ca65f554b8f4badceb50cf87d8e1898d863b7eef
Nick <nickdloucks@outlook.com>
2022-03-29 20:49:23 -0700

    outline Sharp package as mid-ware, not yet dynamic

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import express from 'express';
+import { NextFunction } from 'express-serve-static-core';
 import { promises as fsPromises } from 'fs';
+import sharp from 'sharp';
 
 const app = express();
 const port = process.env.PORT;
@@ -9,17 +11,27 @@ app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
 });
 
-const resize = function(req: express.Request, res: express.Response, next: Function): void {
-    res.send(resizePic());
+const resize = function(req: express.Request, res: express.Response, next: NextFunction): void {
+    // res.send(resizePic());
     next();
 }
-const resizePic = async ()=>{
-    try{
-        const smallerPic = await fsPromises.rename('./images/fjord.jpg', './images/fjord.jpg?width=200&height=200');
-        return smallerPic;
-    }catch(err){
-        console.log(err);
-    }
+// const resizePic = async ()=>{
+//     try{
+//         const smallerPic = await fsPromises.rename('./images/fjord.jpg', './images/fjord.jpg?width=200&height=200');
+//         return smallerPic;
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
+const resizePic = async (imgPath: string) => {
+    let imgInfo;
+    await sharp(imgPath)
+    .resize(300,200)
+    .toFile(`${imgPath}?width=300&height=200`, function(err, info){ 
+        if (err){console.log(err);}
+        imgInfo = info;
+    });
+    return imgInfo;
 }
 
 app.get('/images/fjord.jpg', resize, (req, res)=> {
