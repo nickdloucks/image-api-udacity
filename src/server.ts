bfa799ad8921f44687cb3609d3c01f5234fe4b27
Nick <nickdloucks@outlook.com>
2022-04-27 09:15:46 -0700

    serve static homepage, change port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import express from 'express';
-import { NextFunction } from 'express-serve-static-core';
-import { promises as fsPromises } from 'fs';
+
 import sharp from 'sharp';
 
 const server = express();
 
-const port = process.env.PORT || 3001;
+server.use(express.static('public'));
+
+server.get('/index.html', (req, res)=>{
+    res.send('../public/index.html');
+})
+
+const port = process.env.PORT || 3003;
 
 server.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
